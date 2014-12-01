4ecf12e1d929442841b030db43e4ce23addb6ea2
Kyösti Komulainen <kyosti.komulainen@gmail.com>
2014-12-01 12:17:28 +0200

    Added font type and color support.

--- src/chatclientfx/FXMLDocumentController.java
+++ src/chatclientfx/FXMLDocumentController.java
@@ -15,14 +15,26 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javax.swing.JOptionPane;
 import message.ChatMessage;
 
 /**
@@ -34,8 +46,8 @@ public class FXMLDocumentController implements Initializable {
     private ClientBackEnd backEnd;
     private Thread backThread;
     private String user;
-    private WebEngine engine;
-    private StringBuilder chatHistory = new StringBuilder();
+    //private WebEngine engine;
+    //private StringBuilder chatHistory = new StringBuilder();
     
     @FXML
     Button btnJoin;
@@ -50,11 +62,17 @@ public class FXMLDocumentController implements Initializable {
     TextField chatMessage;
     
     @FXML
-    WebView chatMessageArea;
+    TextFlow chatMessageArea;
     
     @FXML
     ListView userListView;
     
+    @FXML
+    ColorPicker clrPicker;
+    
+    @FXML
+    ChoiceBox cbxFont;
+        
     @FXML
     private void quitChat(ActionEvent e) {
         Platform.exit();
@@ -63,7 +81,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void joinToChat(ActionEvent e) {
         if (userName.getText().isEmpty()) {
-            System.out.println("Pls give username");
+             JOptionPane.showMessageDialog(null, "Please give user name.");
         }
         else {
             this.user = userName.getText();
@@ -78,8 +96,7 @@ public class FXMLDocumentController implements Initializable {
             chatMessage.setDisable(false);
             backEnd.sendMessage(cm);
             chatMessage.requestFocus();
-            
-        }
+         }
     }
     
     @FXML
@@ -97,42 +114,49 @@ public class FXMLDocumentController implements Initializable {
     private void sendChatMessage() {
         ChatMessage cm = new ChatMessage();
         cm.setChatMessage(chatMessage.getText());
+        
+        // Send private message if username is selected from list view
         String privateName = "";
         if (userListView.getSelectionModel().getSelectedItem() != null) {
             privateName = (String) userListView.getSelectionModel().getSelectedItem();
         }
-        // Send private message if username is selected from list view
         if( !privateName.isEmpty() && !privateName.matches("<public>" ) ) {
             cm.setPrivateMessage(true);
             cm.setPrivateName(privateName);
         }
+        
+        // Set message color
         cm.setUserName(this.user);
+        cm.setMessageColor(clrPicker.getValue().toString());
+       
+        // Set message font
+        String tempFont = (String)cbxFont.getValue();
+        cm.setFontType(tempFont);
+        
         backEnd.sendMessage(cm);
         chatMessage.clear();
     }
     
     //public void updateTextArea(String message) {
     public void updateTextArea(ChatMessage cm) {
-        String message = cm.getUserName()+": "+cm.getChatMessage();
+        String message = cm.getUserName()+": "+cm.getChatMessage()+"\n";
+        Text text = new Text(message);
         
-        if(cm.getUserName().equals(this.user)) {
-            if(cm.isPrivateMessage()) {
-                chatHistory.append("<font color='green'><b>"+message+"</b></font><br>");
-            }
-            else {
-                chatHistory.append("<font color='green'>"+message+"</font><br>");
-            }
+        // Get message font
+        String tempFont = cm.getFontType();
+        int fontSize = 12;
+        if(cm.isPrivateMessage()) {
+            text.setFont(Font.font(tempFont,FontWeight.BOLD,fontSize));
         }
         else {
-            if(cm.isPrivateMessage()) {
-                chatHistory.append("<font color='black'><b>"+message+"</b></font><br>");
-            }
-            else {
-                chatHistory.append("<font color='black'>"+message+"</font><br>");
-            }
+            text.setFont(Font.font(tempFont,FontWeight.NORMAL,fontSize));
         }
-        String content = chatHistory.toString();
-        engine.loadContent(content);
+        
+        // Get message color
+        String color = cm.getMessageColor();
+        text.setFill(Color.web(color));
+
+        chatMessageArea.getChildren().addAll(text);
     }
 
     public void updateUserList(String users) {
@@ -164,13 +188,15 @@ public class FXMLDocumentController implements Initializable {
         btnSend.setDisable(true);
         chatMessage.setDisable(true);
         
-        engine = chatMessageArea.getEngine();
+        clrPicker.setValue(Color.BLACK);
+        
+        ObservableList fonts = FXCollections.observableArrayList("Arial", "Tahoma", "Serif");
+        cbxFont.setItems(fonts);
         
         backEnd = new ClientBackEnd(this);
         backThread = new Thread(backEnd);
         backThread.setDaemon(true);
         backThread.start();
-        
     }    
     
 }
